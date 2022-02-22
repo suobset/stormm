6dc0d7f10950f6cfea9b414554a13739d25ec6a8
david.cerutti <david.cerutti@roivant.com>
2022-02-22 16:00:22 -0500

    Add the test directory for the first app

---
+++ apps/Conf/test/AminoAcidTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat > cgen.in << EOF
+&files
+  -p ${OMNI_SOURCE}/test/Namelists/topol/.*.top
+  -c ${OMNI_SOURCE}/test/Namelists/coord/.*.inpcrd
+&end
+EOF
+
+cat >> cgen.in << EOF
+
+EOF
+
+valgrind ${OMNI_HOME}/apps/bin/conformer.omni -i cgen.in
