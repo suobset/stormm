9aa1411c3873dab829f690cda982470f2a98328a
david.cerutti <david.cerutti@roivant.com>
2022-03-01 11:10:24 -0500

    Add the test_isomerization unit testing program

---
+++ test/Structure/test_isomerization.cpp
@@ -0,0 +1,91 @@
+#include "../../src/Chemistry/chemical_features.h"
+#include "../../src/Chemistry/chemistry_enumerators.h"
+#include "../../src/Constants/scaling.h"
+#include "../../src/Constants/symbol_values.h"
+#include "../../src/FileManagement/file_listing.h"
+#include "../../src/Math/matrix_ops.h"
+#include "../../src/Parsing/parse.h"
+#include "../../src/Parsing/polynumeric.h"
+#include "../../src/Potential/scorecard.h"
+#include "../../src/Potential/valence_potential.h"
+#include "../../src/Reporting/error_format.h"
+#include "../../src/Structure/local_arrangement.h"
+#include "../../src/Structure/structure_enumerators.h"
+#include "../../src/Topology/atomgraph.h"
+#include "../../src/Trajectory/coordinateframe.h"
+#include "../../src/Trajectory/phasespace.h"
+#include "../../src/UnitTesting/unit_test.h"
+
+using omni::chemistry::ChemicalFeatures;
+using omni::chemistry::MapRotatableGroups;
+using omni::constants::tiny;
+using omni::diskutil::DrivePathType;
+using omni::diskutil::getDrivePathType;
+using omni::diskutil::osSeparator;
+using omni::errors::rtWarn;
+using omni::math::computeBoxTransform;
+using omni::energy::evaluateBondTerms;
+using omni::energy::evaluateAngleTerms;
+using omni::energy::ScoreCard;
+using omni::topology::AtomGraph;
+using omni::topology::UnitCellType;
+using omni::topology::ValenceKit;
+using omni::trajectory::CoordinateFileKind;
+using omni::trajectory::CoordinateFrame;
+using omni::trajectory::CoordinateFrameReader;
+using omni::trajectory::PhaseSpace;
+using namespace omni::structure;
+using namespace omni::testing;
+
+int main(int argc, char* argv[]) {
+
+  // Some baseline initialization
+  TestEnvironment oe(argc, argv);
+
+  // Section 1
+  section("Rotation about a bond");
+
+  // Section 2
+  section("Selected chiral inversions");
+  
+  // Get a handful of realistic systems
+  const char osc = osSeparator();
+  const std::string base_top_path = oe.getOmniSourcePath() + osc + "test" + osc + "Topology";
+  const std::string base_crd_path = oe.getOmniSourcePath() + osc + "test" + osc + "Trajectory";
+  const std::string drug_top_path = base_top_path + osc + "drug_example.top";
+  const std::string drug_crd_path = base_crd_path + osc + "drug_example.inpcrd";
+  const bool files_exist = (getDrivePathType(drug_top_path) == DrivePathType::FILE &&
+                            getDrivePathType(drug_crd_path) == DrivePathType::FILE);
+  if (files_exist == false) {
+    rtWarn("Files for a drug molecule, in water and inside a periodic box, were not found.  Check "
+           "the $OMNI_SOURCE environment variable to ensure that " + drug_top_path + " and " +
+           drug_crd_path + " become valid paths.  Some tests will be skipped",
+           "test_local_arrangement");
+  }
+  const TestPriority do_tests = (files_exist) ? TestPriority::CRITICAL : TestPriority::ABORT;
+  AtomGraph drug_ag = (files_exist) ? AtomGraph(drug_top_path) : AtomGraph();
+  PhaseSpace drug_ps = (files_exist) ? PhaseSpace(drug_crd_path,
+                                                  CoordinateFileKind::AMBER_INPCRD) :
+                                       PhaseSpace();
+  CoordinateFrameReader drug_cfr = (files_exist) ? CoordinateFrameReader(drug_ps) :
+                                                   CoordinateFrameReader(CoordinateFrame().data());
+  const ChemicalFeatures drug_feat = (files_exist) ? ChemicalFeatures(&drug_ag, drug_cfr,
+                                                                      MapRotatableGroups::YES) :
+                                                     ChemicalFeatures();
+
+  // Compute the bond and angle energies.  These quantities should not change when manipulating
+  // rotatable bonds.
+  ScoreCard sc(1);
+  const ValenceKit<double> vk = drug_ag.getDoublePrecisionValenceKit();
+  const double drug_bond_e = evaluateBondTerms(vk, drug_cfr, &sc, 0);
+  const double drug_angl_e = evaluateAngleTerms(vk, drug_cfr, &sc, 0);
+  section(1);
+  
+  // Rotate about various bonds.  This will generate all sorts of clashes, but bond and angle
+  // energies should be unaffected.
+  
+  
+
+  // Summary evaluation
+  printTestSummary(oe.getVerbosity());
+}
