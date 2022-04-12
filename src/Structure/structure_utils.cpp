a726efdd09d385d3901be6f88af0fb9c03dc7a22
david.cerutti <david.cerutti@roivant.com>
2022-04-11 20:31:12 -0400

    Add the structural utility functions library.

---
+++ src/Structure/structure_utils.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include "Reporting/error_format.h"
+#include "structure_utils.h"
+
+namespace omni {
+namespace structure {
+  
+//-------------------------------------------------------------------------------------------------
+void coordinateBoundsCheck(const int lower_limit, const int upper_limit, const int natom,
+                           const char* caller) {
+  if (lower_limit < 0 || upper_limit < 0 || lower_limit >= natom || upper_limit >= natom) {
+    rtErr("A coordinate set with " + std::to_string(natom) + " particles cannot rotate "
+          "indices " + std::to_string(lower_limit) + " to " + std::to_string(upper_limit) + ".",
+          caller);
+  }
+}
+
+} // namespace structure
+} // namespace omni
