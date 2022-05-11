32db73469ea227b42eb77f7a105f43ce269a89a1
david.cerutti <david.cerutti@roivant.com>
2022-05-11 19:11:41 -0400

    Move the common data type template functions to a .tpp file

--- src/DataTypes/common_types.h
+++ src/DataTypes/common_types.h
@@ -38,58 +38,24 @@ static const size_t bool_type_index = std::type_index(typeid(bool)).hash_code();
 /// \}
   
 /// \brief Test whether some data type is a recognized scalar.
-template <typename T> bool isScalarType() {
-  const size_t ct = std::type_index(typeid(T)).hash_code();
-  return (ct == int_type_index || ct == uint_type_index || ct == double_type_index ||
-          ct == float_type_index || ct == char_type_index || ct == uchar_type_index ||
-          ct == llint_type_index || ct == ullint_type_index || ct == short_type_index ||
-          ct == ushort_type_index || ct == ulint_type_index || ct == bool_type_index);
-}
+template <typename T> bool isScalarType();
 
 /// \brief Test whether some data type is a recognized (signed) integral scalar.
-template <typename T> bool isSignedIntegralScalarType() {
-  const size_t ct = std::type_index(typeid(T)).hash_code();
-  return (ct == int_type_index || ct == char_type_index || ct == llint_type_index ||
-          ct == short_type_index);
-}
+template <typename T> bool isSignedIntegralScalarType();
 
 /// \brief Test whether some data type is a recognized (unsigned) integral scalar.
-template <typename T> bool isUnsignedIntegralScalarType() {
-  const size_t ct = std::type_index(typeid(T)).hash_code();
-  return (ct == uint_type_index || ct == ulint_type_index || ct == uchar_type_index ||
-          ct == ullint_type_index || ct == ushort_type_index || ct == bool_type_index);
-}
+template <typename T> bool isUnsignedIntegralScalarType();
 
 /// \brief Test whether some data type is a recongized floating point number.
-template <typename T> bool isFloatingPointScalarType() {
-  const size_t ct = std::type_index(typeid(T)).hash_code();
-  return (ct == double_type_index || ct == float_type_index);
-}
+template <typename T> bool isFloatingPointScalarType();
 
 /// \brief Produce a platform-independent name by which to identify one of the scalara data types.
-template <typename T> std::string getOmniScalarTypeName() {
-  const size_t ct = std::type_index(typeid(T)).hash_code();
-  if (ct == int_type_index) return "int";
-  else if (ct == uint_type_index) return "unsigned_int";
-  else if (ct == ulint_type_index) return "unsigned_long_int";
-  else if (ct == double_type_index) return "double";
-  else if (ct == float_type_index) return "float";
-  else if (ct == char_type_index) return "char";
-  else if (ct == uchar_type_index) return "unsigned_char";
-  else if (ct == llint_type_index) return "long_long_int";
-  else if (ct == ullint_type_index) return "unsigned_long_long_int";
-  else if (ct == short_type_index) return "short_int";
-  else if (ct == ushort_type_index) return "unsigned_short_int";
-  else if (ct == bool_type_index) return "bool";
-  else {
-    rtErr("Data type " + std::string(std::type_index(typeid(T)).name()) + " is not a recognized "
-	  "scalar type.", "getOmniScalarTypeName");
-  }
-  __builtin_unreachable();
-}
+template <typename T> std::string getOmniScalarTypeName();
   
 } // namespace data_types
 } // namespace omni
+
+#include "common_types.tpp"
 
 namespace omni {
 using data_types::ulint;
