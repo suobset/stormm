6216238628cc9b196d5d4c7dd2256bbb0aa01115
david.cerutti <david.cerutti@roivant.com>
2022-10-13 08:12:25 -0400

    Further elaboration on the ways that an annotate frame might be written

--- src/Trajectory/write_annotated_frame.cpp
+++ src/Trajectory/write_annotated_frame.cpp
@@ -6,16 +6,58 @@ namespace trajectory {
 
 //-------------------------------------------------------------------------------------------------
 void writeFrame(std::ofstream *foutp, const std::string &filename, const TextFile &tf,
+                const double* xcrd, const double* ycrd, const double* zcrd, const int total_width,
+                const int fraction, const std::vector<int> &crd_lines, const int x_start,
+                const int y_start, const int z_start) {
+
+}
+
+//-------------------------------------------------------------------------------------------------
+void writeFrame(std::ofstream *foutp, const std::string &filename, const TextFile &tf,
+                const PhaseSpaceReader &psr, const int total_width, const int fraction,
+                const std::vector<int> &crd_lines, int x_start, int y_start, int z_start) {
+  writeFrame(foutp, filename, tf, psr.xcrd, psr.ycrd, psr.zcrd, total_width, fraction, crd_lines,
+             x_start, y_start, z_start);
+}
+
+//-------------------------------------------------------------------------------------------------
+void writeFrame(std::ofstream *foutp, const std::string &filename, const TextFile &tf,
+                const CoordinateFrameReader &cfr, const int total_width, const int fraction,
+                const std::vector<std::vector<int>> &crd_lines, const int x_start,
+                const int y_start, const int z_start) {
+  writeFrame(foutp, filename, tf, cfr.xcrd, cfr.ycrd, cfr.zcrd, total_width, fraction, crd_lines,
+             x_start, y_start, z_start);
+}
+
+//-------------------------------------------------------------------------------------------------
+void writeFrame(std::ofstream *foutp, const std::string &filename, const TextFile &tf,
                 const PhaseSpace &ps, const int total_width, const int fraction,
                 const std::vector<int> &crd_lines, int x_start, int y_start, int z_start) {
+  const PhaseSpaceReader psr = ps.data();
+  writeFrame(foutp, filename, tf, psr, total_width, fraction, crd_lines, x_start, y_start,
+             z_start);
 }
 
 //-------------------------------------------------------------------------------------------------
 void writeFrame(std::ofstream *foutp, const std::string &filename, const TextFile &tf,
                 const CoordinateFrame &cf, const int total_width, const int fraction,
-                const std::vector<std::vector<int>> &crd_lines, int x_start, int y_start,
-                int z_start) {
+                const std::vector<std::vector<int>> &crd_lines, const int x_start,
+                const int y_start, const int z_start) {
+  const CoordinateFrameReader cfr = cf.data();
+  writeFrame(foutp, filename, tf, psr, total_width, fraction, crd_lines, x_start, y_start,
+             z_start);
 }
-  
+
+//-------------------------------------------------------------------------------------------------
+void writeFrame(std::ofstream *foutp, const std::string &filename, const TextFile &tf,
+                const PhaseSpaceSynthesis &poly_ps, const int total_width, const int fraction,
+                const std::vector<std::vector<int>> &crd_lines, const int x_start,
+                const int y_start, const int z_start) {
+  const PsSynthesisReader poly_psr = poly_ps.data();
+  for (int i = 0; i < poly_psr.system_count; i++) {
+    if (
+  }
+}
+
 } // namespace trajectory
 } // namespace stormm
--- src/Trajectory/write_annotated_frame.h
+++ src/Trajectory/write_annotated_frame.h
@@ -4,12 +4,13 @@
 
 #include <vector>
 #include "copyright.h"
+#include "Synthesis/phasespace_synthesis.h"
 #include "coordinateframe.h"
 #include "phasespace.h"
 
 namespace stormm {
 namespace trajectory {
-    
+
 /// \brief Write one or more frames to a trajectory file in a format-agnostic manner.  This
 ///        function accepts text strings, with all of the information for the various lines of the
 ///        file concatenated together with the requisite carriage returns.
@@ -18,11 +19,25 @@ namespace trajectory {
 ///   - Accept a single TextFile object, assumed to be one frame
 ///   - Accept a vector of TextFile objects, each assumed to be one frame
 ///   - Accept one or more TextFile objects with a corresponding number of CoordinateFrame or
-///     PhaseSpace objects, or an appropriately sized PhaseSpaceSynthesis object and vectors of
-///     line and column indices at which the coordinates exist.  In this case, the coordinates of
-///     the TextFile objects will be replaced with those in the coordinate objects before writing
-///     and the TextFile objects themselves will be unaltered.
+///     PhaseSpace objects, an appropriately sized PhaseSpaceSynthesis object, or abstracts
+///     thereof, and vectors of line and column indices at which the coordinates exist.  In this
+///     case, the coordinates of the TextFile objects will be replaced with those in the coordinate
+///     objects before writing and the TextFile objects themselves will be unaltered.
 /// \{
+void writeFrame(std::ofstream *foutp, const std::string &filename, const TextFile &tf,
+                const double* xcrd, const double* ycrd, const double* zcrd, const int total_width,
+                const int fraction, const std::vector<int> &crd_lines, const int x_start,
+                const int y_start, const int z_start);
+
+void writeFrame(std::ofstream *foutp, const std::string &filename, const TextFile &tf,
+                const PhaseSpaceReader &psr, const int total_width, const int fraction,
+                const std::vector<int> &crd_lines, int x_start, int y_start, int z_start);
+
+void writeFrame(std::ofstream *foutp, const std::string &filename, const TextFile &tf,
+                const CoordinateFrameReader &cfr, const int total_width, const int fraction,
+                const std::vector<std::vector<int>> &crd_lines, const int x_start,
+                const int y_start, const int z_start);
+
 void writeFrame(std::ofstream *foutp, const std::string &filename, const TextFile &tf,
                 const PhaseSpace &ps, int total_width, int fraction,
                 const std::vector<int> &crd_lines, int x_start, int y_start, int z_start);
@@ -31,6 +46,16 @@ void writeFrame(std::ofstream *foutp, const std::string &filename, const TextFil
                 const CoordinateFrame &cf, int total_width, int fraction,
                 const std::vector<std::vector<int>> &crd_lines, int x_start, int y_start,
 		int z_start);
+
+void writeFrame(std::ofstream *foutp, const std::string &filename,
+                const std::vector<TextFile> &tf, const PsSynthesisReader &poly_psr,
+                int total_width, int fraction, const std::vector<std::vector<int>> &crd_lines,
+                int x_start, int y_start, int z_start);
+
+void writeFrame(std::ofstream *foutp, const std::string &filename,
+                const std::vector<TextFile> &tf, const PhaseSpaceSynthesis &poly_ps,
+                int total_width, int fraction, const std::vector<std::vector<int>> &crd_lines,
+                int x_start, int y_start, int z_start);
 /// \}
 
 } // namespace trajectory
