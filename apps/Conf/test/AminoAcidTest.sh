f53347f048368360399056041b5ace989694aba8
David S. Cerutti <dscerutti@gmail.com>
2022-11-17 17:12:07 -0500

    Update the test case for the conformer generator in preparation to do the core masking

--- apps/Conf/test/AminoAcidTest.sh
+++ apps/Conf/test/AminoAcidTest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "&files" > cgen.in
-for SYS in gly_lys gly_gly ala arg gly lys phe pro trp tyr gly_ala gly_arg gly_gly gly_lys gly_phe \
-           gly_pro gly_trp gly_tyr ; do
+for SYS in gly_lys gly_gly ala arg gly lys phe pro trp tyr gly_ala gly_arg gly_gly gly_lys \
+           gly_phe gly_pro gly_trp gly_tyr ; do
   echo "  -sys { -p ${STORMM_SOURCE}/test/Namelists/topol/${SYS}.top" >> cgen.in
   echo "         -c ${STORMM_SOURCE}/test/Namelists/coord/${SYS}.inpcrd }" >> cgen.in
 done
@@ -12,6 +12,7 @@ cat >> cgen.in << EOF
 &conformer
   rotation_samples 6,
   max_system_trials 1024,
+  core_mask { atoms "@N,CA,C & !(:ACE,NME)" }
 &end
 
 &solvent
